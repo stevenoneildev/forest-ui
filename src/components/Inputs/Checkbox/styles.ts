15f9553a173ee30fdd7a59bdce8a23ca7e4bb1f1
Steven O'Neil <stevenoneil.work@gmail.com>
2025-01-19 14:28:23 +0000

    CheckboxContained

--- src/components/Inputs/Checkbox/styles.ts
+++ src/components/Inputs/Checkbox/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StyledLabel = styled.label<{ variant: string }>`
     display: flex;
@@ -55,7 +55,112 @@ export const StyledLabel = styled.label<{ variant: string }>`
                 return theme.typography.bodyM.lineHeight;
         }
     }};
-`
+`;
+
+export const StyledCheckboxContainedLabel = styled.label<{ variant: string }>`
+    display: flex;
+    flex-direction: row;
+    margin: 0;
+    cursor: pointer;
+    border-radius: ${({ theme }) => theme.borderRadius.borderRadiusM};
+    border: 1px solid ${({ theme }) => theme.colors.border.neutral};
+    padding-left: ${({ theme, variant }) => {
+        switch (variant) {
+            case "medium":
+                return theme.spacing.spacingS;
+            case "small":
+                return theme.spacing.spacingXS;           
+            default:
+                return theme.spacing.spacingS;
+        }
+    }};
+    padding-right: ${({ theme, variant }) => {
+        switch (variant) {
+            case "medium":
+                return theme.spacing.spacingS;
+            case "small":
+                return theme.spacing.spacingXS;           
+            default:
+                return theme.spacing.spacingS;
+        }
+    }};
+    padding-top: ${({ theme, variant }) => {
+        switch (variant) {
+            case "medium":
+                return theme.spacing.spacingS;
+            case "small":
+                return theme.spacing.spacingXS;           
+            default:
+                return theme.spacing.spacingS;
+        }
+    }};
+    padding-bottom: ${({ theme, variant }) => {
+        switch (variant) {
+            case "medium":
+                return theme.spacing.spacingS;
+            case "small":
+                return theme.spacing.spacingXS;           
+            default:
+                return theme.spacing.spacingS;
+        }
+    }};
+    gap: ${({ theme, variant }) => {
+        switch (variant) {
+            case "medium":
+                return theme.spacing.spacingS;
+            case "small":
+                return theme.spacing.spacingXS;           
+            default:
+                return theme.spacing.spacingS;
+        }
+    }};
+    font-family: ${({ theme, variant }) => {
+        switch (variant) {
+            case "medium":
+                return theme.typography.bodyM.fontFamily;
+            case "small":
+                return theme.typography.bodyS.fontFamily;             
+            default:
+                return theme.typography.bodyM.fontFamily;
+        }
+    }};
+    font-size: ${({ theme, variant }) => {
+        switch (variant) {
+            case "medium":
+                return theme.typography.bodyM.fontSize;
+            case "small":
+                return theme.typography.bodyS.fontSize;             
+            default:
+                return theme.typography.bodyM.fontSize;
+        }
+    }};
+    font-weight: ${({ theme, variant }) => {
+        switch (variant) {
+            case "medium":
+                return theme.typography.bodyM.fontWeight;
+            case "small":
+                return theme.typography.bodyS.fontWeight;             
+            default:
+                return theme.typography.bodyM.fontWeight;
+        }
+    }};
+    line-height: ${({ theme, variant }) => {
+        switch (variant) {
+            case "medium":
+                return theme.typography.bodyM.lineHeight;
+            case "small":
+                return theme.typography.bodyS.lineHeight;             
+            default:
+                return theme.typography.bodyM.lineHeight;
+        }
+    }};
+    &:hover {
+        border: 1px solid ${({ theme }) => theme.colors.border.neutralHover};
+    };
+    &:active {
+        border: 1px solid ${({ theme }) => theme.colors.border.neutralActive};
+    };
+`;
 
 export const StyledCheckbox = styled.input<{ variant: string }>`
     appearance: none;
@@ -65,26 +170,26 @@ export const StyledCheckbox = styled.input<{ variant: string }>`
     border-radius: ${({ theme }) => theme.borderRadius.borderRadiusS};
     color: ${({ theme }) => theme.colors.foreground.onPrimary};    
     margin: 0;
-        height: ${({ theme, variant }) => {
-            switch (variant) {
-                case "medium":
-                    return theme.size.sizeM;
-                case "small":
-                    return theme.size.sizeS;           
-                default:
-                    return theme.size.sizeM;
-            }
-        }};
-        width: ${({ theme, variant }) => {
-            switch (variant) {
-                case "medium":
-                    return theme.size.sizeM;
-                case "small":
-                    return theme.size.sizeS;           
-                default:
-                    return theme.size.sizeM;
-            }
-        }};
+    height: ${({ theme, variant }) => {
+        switch (variant) {
+            case "medium":
+                return theme.size.sizeM;
+            case "small":
+                return theme.size.sizeS;           
+            default:
+                return theme.size.sizeM;
+        }
+    }};
+    width: ${({ theme, variant }) => {
+        switch (variant) {
+            case "medium":
+                return theme.size.sizeM;
+            case "small":
+                return theme.size.sizeS;           
+            default:
+                return theme.size.sizeM;
+        }
+    }};
     &:hover {
         border: 1px solid ${({ theme }) => theme.colors.border.neutralHover};
     }
@@ -96,7 +201,7 @@ export const StyledCheckbox = styled.input<{ variant: string }>`
         border: none;
     }
     &:checked::after {
-        content: '✔'; /* You can replace this with a custom icon */
+        content: '✔';
         display: block;
         color: ${({ theme }) => theme.colors.foreground.onPrimary};
         font-size: ${({ theme, variant }) => {
@@ -136,4 +241,4 @@ export const StyledCheckboxGroup = styled.div<{ variant: string }>`
                 return theme.spacing.spacingS;
         }
     }};
-`
+`;
